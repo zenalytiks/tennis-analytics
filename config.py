511d52584a985554940e7e5387939dd8bed4bfe7
Waleed Malik <zenalytiks@gmail.com>
2025-09-18 17:52:48 +0500

    update paths

--- config.py
+++ config.py
@@ -11,5 +11,6 @@ app = dash.Dash(
     meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"}
     ],
+    url_base_pathname='/tennis-analytics/',
     suppress_callback_exceptions=True
 )
